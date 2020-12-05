0e6c95258b63fe6042028ab6950db0c3b8b67179
richard1230 <yongjinhu1230@gmail.com>
2020-12-05 18:12:39 +0800

    normalize.css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>Hello</h1>
+        <h2>Hello</h2>
+        <h3>Hello</h3>
+
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
